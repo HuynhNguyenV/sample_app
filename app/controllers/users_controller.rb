08b969b604dbb2ad4320639ea9795d6aaf584349
Huynh Nguyen V <nguyenviethuynh1996.vih@gmail.com>
2019-06-18 13:59:39 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t "user_not_found"
-    redirect_to root_path
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -21,9 +25,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "user_delete"
+    else
+      flash[:danger] = t "user_delete_error"
+    end
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
       :password_confirmation)
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    render file: "public/404.html", status: :not_found, layout: false
   end
 end
